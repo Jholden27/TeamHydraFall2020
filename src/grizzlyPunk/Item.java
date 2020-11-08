e8056eb8a4831f0ab8a69c29300dbd98e1764c0f
Taylor <tmarsh@ggc.edu>
2020-11-08 02:12:50 -0500

    Taylor working on methods

--- src/grizzlyPunk/Item.java
+++ src/grizzlyPunk/Item.java
@@ -5,17 +5,19 @@ public class Item {
 	private String itemName;
 	private String itemDescription;	
 	private String itemType;
-	private String itemNumericValue;
+	private int itemNumericValue;
+	private boolean isEquipped;
 	
-	public Item(String itemID, String itemName, String itemDescription, String itemType, String itemNumericValue) {
+	public Item(String itemID, String itemName, String itemDescription, String itemType, int itemNumericValue,
+			boolean isEquipped) {
 		super();
 		this.itemID = itemID;
 		this.itemName = itemName;
 		this.itemDescription = itemDescription;
 		this.itemType = itemType;
 		this.itemNumericValue = itemNumericValue;
+		this.isEquipped = isEquipped;
 	}
-	
 	public String getItemID() {
 		return itemID;
 	}
@@ -40,16 +42,24 @@ public class Item {
 	public void setItemType(String itemType) {
 		this.itemType = itemType;
 	}
-	public String getItemNumericValue() {
+	
+	public int getItemNumericValue() {
 		return itemNumericValue;
 	}
-	public void setItemNumericValue(String itemNumericValue) {
+	public void setItemNumericValue(int itemNumericValue) {
 		this.itemNumericValue = itemNumericValue;
 	}
-	 
+	public boolean isEquipped() {
+		return isEquipped;
+	}
+	public void setEquipped(boolean isEquipped) {
+		this.isEquipped = isEquipped;
+	}
 	@Override
 	public String toString() {
 		return "Item [itemID=" + itemID + ", itemName=" + itemName + ", itemDescription=" + itemDescription
-				+ ", itemType=" + itemType + ", itemNumericValue=" + itemNumericValue + "]";
+				+ ", itemType=" + itemType + ", itemNumericValue=" + itemNumericValue + ", isEquipped=" + isEquipped
+				+ "]";
 	}
+
 }
